2f781b5b6a0dfcc093c4cf0938a92c2592d3688d
DeivyAyala <107268555+DeivyAyala@users.noreply.github.com>
2024-04-27 19:12:14 -0500

    Add files via upload

---
+++ Clase#3/0_Archivo_1.py
@@ -0,0 +1,10 @@
+
+# Esta es una variable 
+variable = 8
+my_string = "hola"
+#Muestra la informacion en la terminal 
+print("Hola Mundo");
+print(type(variable));
+print(type(8.5));
+print(my_string.upper());
+
